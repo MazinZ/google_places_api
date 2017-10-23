38af204f0852a393fa571401349d5a7ba6a0a1dc
Mazin Zakaria <mazinzakaria1@gmail.com>
2017-10-22 23:35:05 -0400

    cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,28 @@ var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
-
 var dist = 'app/build/';
-var rootJsFiles = 'app/js/*.js';
-var appJsFiles = 'app/js/**/*.js';
+var jsFiles = ['app/js/*.js'];
+var scssFiles = ['app/scss/*.scss'];
 
 gulp.task('sass', function() {
-  return gulp.src(['app/scss/**/*.scss', 'app/scss/*.scss'])
+  return gulp.src(scssFiles)
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('main.css'))
     .pipe(gulp.dest(dist));
 });
 
 gulp.task('js', function() {
-  return gulp.src(['!app/js/external/**/*', rootJsFiles, appJsFiles, 'app/js/**/**/*.js'])
+  return gulp.src(jsFiles)
     .pipe(concat('all.js'))
     .pipe(gulp.dest(dist));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['app/js/**/*', 'app/js/*', 'app/js/**/**/*.js'], ['js']);
-  gulp.watch(['app/scss/**/*'], ['sass']);
+  gulp.watch(jsFiles, ['js']);
+  gulp.watch(scssFiles, ['sass']);
 });
 
 gulp.task('default', ['js', 'sass'], function() {
-  console.log('Building and watching.')
+  console.log('Build finished.');
 });
